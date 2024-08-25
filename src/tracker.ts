e3ceb925c47a83853b223e8fc20889b8b324a981
Andrew Makarov <me@r3nya.ru>
2024-08-25 15:23:14 +0200

    Setup tracker with CollectorURL

--- src/tracker.ts
+++ src/tracker.ts
@@ -1,12 +1,18 @@
-// import { trackSelfDescribingEvent } from '@snowplow/react-native-tracker';
+import { createTracker } from '@snowplow/react-native-tracker';
 
 import CASES from './constants/cases';
 import DATA from './constants/data';
 
+const COLLECTOR_URL = 'https://stream.datago.ru/collector';
+
+const tracker = createTracker('appTracker', {
+  endpoint: COLLECTOR_URL,
+});
+
 export const trackEventByCase = (caseId: number) => {
   const eventCase = CASES.find((c) => c.id === caseId);
+
   if (eventCase) {
-    console.log(eventCase, DATA[caseId]);
-    // trackSelfDescribingEvent(DATA[caseId]);
+    tracker.trackSelfDescribingEvent(DATA[caseId]);
   }
 };
